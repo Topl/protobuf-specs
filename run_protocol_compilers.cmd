4eb1cc981379852fa637e96f6f137f026a249182
mgrand <m.grand@topl.me>
2022-11-02 10:35:21 -0400

    compile genus too

--- run_protocol_compilers.cmd
+++ run_protocol_compilers.cmd
@@ -3,4 +3,4 @@
 mkdir java 2>z
 mkdir js 2>z
 mkdir python 2>z
-protoc --js_out=js --python_out=python --java_out=java --proto_path=protobuf protobuf/bifrost/bifrost_rpc.proto
+protoc --js_out=js --python_out=python --java_out=java --proto_path=protobuf protobuf/bifrost/bifrost_rpc.proto protobuf/genus/genus_rpc.proto
