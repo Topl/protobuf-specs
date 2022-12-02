e1e379d2acaa7d6e27b6e1265404163bf506533d
Fernando Torterolo <f.torterolo@topl.me>
2022-12-02 10:47:32 -0300

    remove top level folder

--- run_protocol_compilers.sh
+++ run_protocol_compilers.sh
@@ -1,12 +1,11 @@
-# @rem sifdj sifdj s ask mark why we need to delete this? see windows cmd
-
-mkdir java 2>z
-mkdir js 2>z
-mkdir python 2>z
+mkdir generated 2>>log
+mkdir generated/java 2>>log
+mkdir generated/js 2>>log
+mkdir generated/python 2>>log
 
 # TODO --js_out=js generation is not allowed with optionals
 # --experimental_allow_proto3_optional
 # ➜  protobuf-specs git:(main) ✗ protoc --version -> libprotoc 3.12.4
 
-protoc --experimental_allow_proto3_optional --python_out=python --java_out=java --proto_path=protobuf protobuf/node/bifrost_rpc.proto protobuf/genus/genus_rpc.proto
+protoc --experimental_allow_proto3_optional --python_out=generated/python --java_out=generated/java node/bifrost_rpc.proto genus/genus_rpc.proto
 
