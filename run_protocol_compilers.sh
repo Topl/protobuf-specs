bf0254ca07197d649b1191431797bdb909040e89
Sean Cheatham <SeanCheatham@users.noreply.github.com>
2022-12-06 19:08:01 +0000

    devcontainer updates

--- run_protocol_compilers.sh
+++ run_protocol_compilers.sh
@@ -2,6 +2,7 @@ mkdir generated 2>>log
 mkdir generated/java 2>>log
 mkdir generated/js 2>>log
 mkdir generated/python 2>>log
+mkdir generated/dart 2>>log
 
 # TODO --js_out=js generation is not allowed with optionals
 # --experimental_allow_proto3_optional
@@ -11,5 +12,6 @@ protoc \
     --experimental_allow_proto3_optional \
     --python_out=generated/python \
     --java_out=generated/java \
+    --dart_out=generated/dart \
     -Iproto/ \
     $(find . -iname "*.proto")
